4da37b1e1d990fa7b7a29da29dd33b76d3b0b1a4
petrgali <opelliek@gmail.com>
2021-09-12 16:12:10 +0600

    fead: add hanoi && season solution

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!'
+  if (!(date instanceof Date) || date.toString !== new Date().toString) throw new Error("Invalid date!")
+
+  const month = date.getMonth();
+  switch (true) {
+    case (2 <= month && month <= 4):
+      return "spring"
+    case (5 <= month && month <= 7):
+      return "summer"
+    case (8 <= month && month <= 10):
+      return "fall"
+    default:
+      return "winter"
+  }
 }
